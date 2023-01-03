b9174ecbea35843bc63cd95cc9b41195a036bde3
SJ-WJ <sirajwahaj.ibrahim@gmail.com>
2023-01-02 20:17:50 -0700

    Replacing the lorem ipsum for the Chef's words section

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -17,9 +17,9 @@ const Chef = () => (
       <div className="app__chef-content">
         <div className="app__chef-content_quote">
           <img src={images.quote} alt="quote" />
-          <p className='p__opensans'>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+          <p className='p__opensans'>Gericht: Elevating the art of dining.</p>
         </div>
-        <p className='p__opensans'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora esse temporibus quidem non vitae, hic expedita eum unde magnam consectetur voluptate fugit deleniti corrupti suscipit doloremque dicta minus, consequatur eveniet?</p>
+        <p className='p__opensans'>As the main chef and founder of Gericht, I am proud to offer our guests a dining experience that showcases the diverse flavors and techniques of international cuisines. At Gericht, we use only the highest quality ingredients, and we are always seeking to innovate and bring new and exciting dishes to our menu. I am deeply passionate about food and the culinary arts, and I am committed to providing our guests with exceptional service and an unparalleled dining experience. I look forward to the opportunity to serve you at Gericht.</p>
       </div>
 
       <div className="app__chef-sign">
